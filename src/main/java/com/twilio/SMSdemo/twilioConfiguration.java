645c3d5033ba2ddaed25207cf5612ffe16ead8d4
Muziwandile Nkomo <muziwandilenko@gmail.com>
2022-09-26 22:53:13 +0200

    added constructor for fields and also getters & setters

--- src/main/java/com/twilio/SMSdemo/twilioConfiguration.java
+++ src/main/java/com/twilio/SMSdemo/twilioConfiguration.java
@@ -6,5 +6,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("twilio")
 public class twilioConfiguration {
+    private String accountSid;
+    private String authToken;
+    private String trailnumber;
 
+    //constructor
+    public twilioConfiguration() {
+
+    }
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getTrailnumber() {
+        return trailnumber;
+    }
+
+    public void setTrailnumber(String trailnumber) {
+        this.trailnumber = trailnumber;
+    }
 }
