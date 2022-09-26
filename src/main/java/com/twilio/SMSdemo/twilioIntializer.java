7c1a2794d309b9c5674a6b05b81b8f9c5ac4e731
Muziwandile Nkomo <muziwandilenko@gmail.com>
2022-09-26 23:07:47 +0200

    dependancyinjection from twilioconfig class & initialising app

--- src/main/java/com/twilio/SMSdemo/twilioIntializer.java
+++ src/main/java/com/twilio/SMSdemo/twilioIntializer.java
@@ -1,2 +1,25 @@
-package com.twilio.SMSdemo;public class twilioIntializer {
+package com.twilio.SMSdemo;
+
+import com.twilio.Twilio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class twilioIntializer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(twilioIntializer.class);
+    private  final  twilioConfiguration twilioConfiguration;
+
+    @Autowired
+    public twilioIntializer(com.twilio.SMSdemo.twilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
+        Twilio.init(twilioConfiguration.getAccountSid(),
+                twilioConfiguration.getAuthToken());
+        LOGGER.info("Twilio init....... with account sid {}", twilioConfiguration.getAccountSid());
+    }
+
+
+
 }
