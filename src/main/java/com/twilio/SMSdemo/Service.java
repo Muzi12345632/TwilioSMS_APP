74c783eb0239e111cedca07d1d50006d68a910d0
Muziwandile Nkomo <muziwandilenko@gmail.com>
2022-09-29 00:36:42 +0200

    service thats gonna send sms

--- src/main/java/com/twilio/SMSdemo/Service.java
+++ src/main/java/com/twilio/SMSdemo/Service.java
@@ -1,2 +1,16 @@
-package com.twilio.SMSdemo;public class Service {
+package com.twilio.SMSdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Service {
+
+    private final Smsend smsend; //uses interface
+    @Autowired
+    public Service(@Qualifier("twilio") com.twilio.SMSdemo.Smsend smsend) {
+        this.smsend = smsend;
+    }
+    public  void sendSms(SmsRequest smsRequest){
+        smsend.sendSms(smsRequest);
+    }
 }
