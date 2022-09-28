616b248f6fe9dcd080f4c705eb98565e77f794ea
Muziwandile Nkomo <muziwandilenko@gmail.com>
2022-09-29 00:40:37 +0200

    includes properties for message

--- src/main/java/com/twilio/SMSdemo/SmsRequest.java
+++ src/main/java/com/twilio/SMSdemo/SmsRequest.java
@@ -1,2 +1,31 @@
-package com.twilio.SMSdemo;public class SmsRequest {
+package com.twilio.SMSdemo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SmsRequest {
+    //Phonenumber validation not allow null values
+    private  final String phonenumber; //destination phonenumber
+    private final String message;
+
+    public SmsRequest(@JsonProperty("phonenumber") String phonenumber,
+                      @JsonProperty("message") String message) {
+        this.phonenumber = phonenumber;
+        this.message = message;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "SmsRequest{" +
+                "phonenumber='" + phonenumber + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
